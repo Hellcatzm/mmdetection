cce6306b663f5fc37915aa6db779f7f192f529eb
叠加态的猫 <18811474740@163.com>
2019-08-05 16:06:08 +0800

    data augmentation

--- mmdet/datasets/carbonate.py
+++ mmdet/datasets/carbonate.py
@@ -1,5 +1,232 @@
+import os.path as osp
+
+import mmcv
+import numpy as np
+from mmcv.parallel import DataContainer as DC
+import imgaug as ia
+import imgaug.augmenters as iaa
+
 from .coco import CocoDataset
+from .utils import to_tensor, random_scale
+import matplotlib.pyplot as plt
+
+
+def activator_heatmaps(images, augmenter, parents, default):
+    # mask和segm限定值为{0, 1}, 很多增强强制不能使用(实际上也没有必要使用)
+    if augmenter.name in ["GaussianBlur", "Dropout",]:
+        return False
+    else:
+        # default value for all other augmenters
+        return default
+
+# Sometimes(0.5, ...) applies the given augmenter in 50% of all cases,
+# e.g. Sometimes(0.5, GaussianBlur(0.3)) would blur roughly every second image.
+sometimes = lambda aug: iaa.Sometimes(0.5, aug)
 
 
 class CarbonateDataset(CocoDataset):
     CLASSES = ('grain', 'cement')
+
+
+    # Define our sequence of augmentation steps that will be applied to every image
+    # All augmenters with per_channel=0.5 will sample one value _per image_
+    # in 50% of all cases. In all other cases they will sample new values
+    # _per channel_.
+    seq = iaa.Sequential(
+        [
+            # 原程序已经实现flip和随机crop，此处主要针对色彩等进行增强即可
+            # # apply the following augmenters to most images
+            # iaa.Fliplr(0.5),  # horizontally flip 50% of all images
+            # iaa.Flipud(0.2),  # vertically flip 20% of all images
+            # # crop images by -5% to 10% of their height/width
+            # sometimes(iaa.CropAndPad(
+            #     percent=(-0.05, 0.1),
+            #     pad_mode=ia.ALL,
+            #     pad_cval=(0, 255)
+            # )),
+            # sometimes(iaa.Affine(
+            #     scale={"x": (0.8, 1.2), "y": (0.8, 1.2)},
+            #     # scale images to 80-120% of their size, individually per axis
+            #     translate_percent={"x": (-0.2, 0.2), "y": (-0.2, 0.2)},  # translate by -20 to +20 percent (per axis)
+            #     rotate=(-45, 45),  # rotate by -45 to +45 degrees
+            #     shear=(-16, 16),  # shear by -16 to +16 degrees
+            #     order=[0, 1],  # use nearest neighbour or bilinear interpolation (fast)
+            #     cval=(0, 255),  # if mode is constant, use a cval between 0 and 255
+            #     mode=ia.ALL  # use any of scikit-image's warping modes (see 2nd image from the top for examples)
+            # )),
+            # execute 0 to 5 of the following (less important) augmenters per image
+            # don't execute all of them, as that would often be way too strong
+            iaa.SomeOf((0, 5),
+                       [
+                           sometimes(iaa.Superpixels(p_replace=(0, 1.0), n_segments=(20, 200))),
+                           # convert images into their superpixel representation
+                           iaa.OneOf([
+                               iaa.GaussianBlur((0, 3.0)),  # blur images with a sigma between 0 and 3.0
+                               iaa.AverageBlur(k=(2, 7)),
+                               # blur image using local means with kernel sizes between 2 and 7
+                               iaa.MedianBlur(k=(3, 11)),
+                               # blur image using local medians with kernel sizes between 2 and 7
+                           ]),
+                           iaa.Sharpen(alpha=(0, 1.0), lightness=(0.75, 1.5)),  # sharpen images
+                           iaa.Emboss(alpha=(0, 1.0), strength=(0, 2.0)),  # emboss images
+                           # search either for all edges or for directed edges,
+                           # blend the result with the original image using a blobby mask
+                           iaa.SimplexNoiseAlpha(iaa.OneOf([
+                               iaa.EdgeDetect(alpha=(0.5, 1.0)),
+                               iaa.DirectedEdgeDetect(alpha=(0.5, 1.0), direction=(0.0, 1.0)),
+                           ])),
+                           iaa.AdditiveGaussianNoise(loc=0, scale=(0.0, 0.05 * 255), per_channel=0.5),
+                           # add gaussian noise to images
+                           iaa.OneOf([
+                               iaa.Dropout((0.01, 0.1), per_channel=0.5),  # randomly remove up to 10% of the pixels
+                               iaa.CoarseDropout((0.03, 0.15), size_percent=(0.02, 0.05), per_channel=0.2),
+                           ]),
+                           iaa.Invert(0.05, per_channel=True),  # invert color channels
+                           iaa.Add((-10, 10), per_channel=0.5),
+                           # change brightness of images (by -10 to 10 of original value)
+                           iaa.AddToHueAndSaturation((-20, 20)),  # change hue and saturation
+                           # either change the brightness of the whole image (sometimes
+                           # per channel) or change the brightness of subareas
+                           iaa.OneOf([
+                               iaa.Multiply((0.5, 1.5), per_channel=0.5),
+                               iaa.FrequencyNoiseAlpha(
+                                   exponent=(-4, 0),
+                                   first=iaa.Multiply((0.5, 1.5), per_channel=True),
+                                   second=iaa.ContrastNormalization((0.5, 2.0))
+                               )
+                           ]),
+                           iaa.ContrastNormalization((0.5, 2.0), per_channel=0.5),  # improve or worsen the contrast
+                           iaa.Grayscale(alpha=(0.0, 1.0)),
+                           sometimes(iaa.ElasticTransformation(alpha=(0.5, 3.5), sigma=0.25)),
+                           # move pixels locally around (with random strengths)
+                           sometimes(iaa.PiecewiseAffine(scale=(0.01, 0.05))),
+                           # sometimes move parts of the image around
+                           sometimes(iaa.PerspectiveTransform(scale=(0.01, 0.1))),
+
+                           iaa.OneOf([
+                               iaa.LogContrast(per_channel=True),
+                               iaa.SigmoidContrast(per_channel=True),
+                               iaa.GammaContrast(per_channel=True),
+                               iaa.LinearContrast(per_channel=True)
+                           ]),
+                       ],
+                       random_order=True
+                       )
+        ],
+        random_order=True
+    )
+
+    hooks_heatmaps = ia.HooksImages(activator=activator_heatmaps)
+
+    def prepare_train_img(self, idx):
+        img_info = self.img_infos[idx]
+        # load image
+        img = mmcv.imread(osp.join(self.img_prefix, img_info['filename']))
+        # load proposals if necessary
+        if self.proposals is not None:
+            proposals = self.proposals[idx][:self.num_max_proposals]
+            # TODO: Handle empty proposals properly. Currently images with
+            # no proposals are just ignored, but they can be used for
+            # training in concept.
+            if len(proposals) == 0:
+                return None
+            if not (proposals.shape[1] == 4 or proposals.shape[1] == 5):
+                raise AssertionError(
+                    'proposals should have shapes (n, 4) or (n, 5), '
+                    'but found {}'.format(proposals.shape))
+            if proposals.shape[1] == 5:
+                scores = proposals[:, 4, None]
+                proposals = proposals[:, :4]
+            else:
+                scores = None
+
+        ann = self.get_ann_info(idx)
+        gt_bboxes = ann['bboxes']
+        gt_labels = ann['labels']
+        if self.with_crowd:
+            gt_bboxes_ignore = ann['bboxes_ignore']
+
+        # skip the image if there is no valid gt bbox
+        if len(gt_bboxes) == 0:
+            return None
+
+        # extra augmentation
+        if self.extra_aug is not None:
+            img, gt_bboxes, gt_labels = self.extra_aug(img, gt_bboxes,
+                                                       gt_labels)
+
+        seq_det = self.seq.to_deterministic() # call this for each batch again, NOT only once at the start
+        img = seq_det.augment_images(img[None, ...])[0]
+
+        # apply transforms
+        flip = True if np.random.rand() < self.flip_ratio else False
+        # randomly sample a scale
+        img_scale = random_scale(self.img_scales, self.multiscale_mode)
+        img, img_shape, pad_shape, scale_factor = self.img_transform(
+            img, img_scale, flip, keep_ratio=self.resize_keep_ratio)
+        img = img.copy()
+        if self.proposals is not None:
+            proposals = self.bbox_transform(proposals, img_shape, scale_factor,
+                                            flip)
+            proposals = np.hstack(
+                [proposals, scores]) if scores is not None else proposals
+        gt_bboxes = self.bbox_transform(gt_bboxes, img_shape, scale_factor,
+                                        flip)
+        if self.with_crowd:
+            gt_bboxes_ignore = self.bbox_transform(gt_bboxes_ignore, img_shape,
+                                                   scale_factor, flip)
+        if self.with_mask:
+            gt_masks = self.mask_transform(ann['masks'], pad_shape,
+                                           scale_factor, flip)
+
+        ori_shape = (img_info['height'], img_info['width'], 3)
+        img_meta = dict(
+            ori_shape=ori_shape,
+            img_shape=img_shape,
+            pad_shape=pad_shape,
+            scale_factor=scale_factor,
+            flip=flip)
+
+        if self.with_seg:
+            gt_seg = mmcv.imread(
+                osp.join(self.seg_prefix, img_info['file_name'].replace(
+                    'jpg', 'png')),
+                flag='unchanged')
+            gt_seg = self.seg_transform(gt_seg.squeeze(), img_scale, flip)
+            gt_seg = mmcv.imrescale(
+                gt_seg, self.seg_scale_factor, interpolation='nearest')
+            gt_seg = gt_seg[None, ...]
+
+        # 由于gt_bboxes的关系，位置变换增强会比较麻烦，不进行位置变换时mask和seg都没必要增强
+        # gt_masks = seq_det.augment_images(gt_masks.transpose(1, 2, 0)[None, ...],
+        #                                   hooks=self.hooks_heatmaps).transpose(0, 3, 1, 2)[0]
+        # gt_seg = seq_det.augment_images(gt_seg.transpose(1, 2, 0)[None, ...],
+        #                                 hooks=self.hooks_heatmaps).transpose(0, 3, 1, 2)[0]
+
+        # plt.subplot(311)
+        # plt.imshow(img[0, ...])
+        # plt.subplot(312)
+        # plt.imshow(gt_masks[0, ...])
+        # plt.subplot(313)
+        # plt.imshow(gt_seg[0, ...])
+        # plt.show()
+        # print(img.shape)        # c, h, w
+        # print(gt_bboxes.shape)  # n, 4
+        # print(gt_masks.shape)   # n, h, w
+        # print(gt_seg.shape)     # 1, h', w'
+
+        data = dict(
+            img=DC(to_tensor(img), stack=True),
+            img_meta=DC(img_meta, cpu_only=True),
+            gt_bboxes=DC(to_tensor(gt_bboxes)))
+        if self.proposals is not None:
+            data['proposals'] = DC(to_tensor(proposals))
+        if self.with_label:
+            data['gt_labels'] = DC(to_tensor(gt_labels))
+        if self.with_crowd:
+            data['gt_bboxes_ignore'] = DC(to_tensor(gt_bboxes_ignore))
+        if self.with_mask:
+            data['gt_masks'] = DC(gt_masks, cpu_only=True)
+        if self.with_seg:
+            data['gt_semantic_seg'] = DC(to_tensor(gt_seg), stack=True)
+        return data
